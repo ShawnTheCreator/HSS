aadea28a1a74d7b8047e829ffebe0611614ab2e3
Shawn <shawnchareka7@gmail.com>
2025-06-01 21:21:05 +0200

    main

--- hss-frontend/src/pages/Login.tsx
+++ hss-frontend/src/pages/Login.tsx
@@ -37,7 +37,7 @@ const Login = () => {
 
   const onSubmit = async (data: FormValues) => {
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://hss-backend.onrender.com/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
