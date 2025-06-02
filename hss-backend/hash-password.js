2d133f0949f3f6c8f8950f577f8538ef4c93da72
Shawn <shawnchareka7@gmail.com>
2025-06-02 14:31:31 +0200

    git

--- hss-backend/hash-password.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-(async () => {
-  const password = 'AdminPass123!';
-  const hash = await bcrypt.hash(password, 10);
-  console.log('Hashed password:', hash);
-})();
